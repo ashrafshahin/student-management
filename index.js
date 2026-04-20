edb14e5d10ed23df15b9c228a82d53aa9bb32caf
Ashraf Shahin <ashraf.hossein.web@gmail.com>
2026-04-20 06:53:46 +0100

    Password bcrypt work - bcrypt.hashSync(password, 10) and compare while login - bcrypt.comareSync(userPassword, databasePassword) - duita password compare kore success hole login korbe...

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ console.log(`Students ${process.env.STUDENT}`)
 const express = require('express');
 const { default: mongoose } = require('mongoose');
 const dbConnection = require('./src/config/dbConnection');
-const { createUser, loginUser } = require('./src/controllers/authController');
+const { loginUser, registrationController } = require('./src/controllers/authController');
 
 // basic way to connect database... username-password dete hobe
 // mongoose.connect('mongodb+srv://username:password@clustershahin.nicn5ni.mongodb.net/student-management?appName=ClusterShahin').then(() => {
@@ -16,7 +16,7 @@ dbConnection()
 
 app.use(express.json())
 
-app.post('/registration', createUser)
+app.post('/registration', registrationController)
 app.post('/login', loginUser)
 
 
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,26 +1,55 @@
 const User = require("../models/userSchema");
+const bcrypt = require("bcryptjs");
 
-const createUser = async (req, res) => {
+
+const registrationController = async (req, res) => {
     const { username, email, password } = req.body
     
-    const data = await new User({
-        username,
-        email,
-        password
+    // email ta used kina dekbo...
+    const existingUser = await User.findOne({ email: email });
+    if (existingUser) {
+        res.status(409).json({success:false, message: "Email already exists..."})
+    }
+    // password bcrypt korbo
+    const hashPassword = bcrypt.hashSync(password, 10);
+    console.log(hashPassword, "hash chack...");
+    
+    const createUser = await new User({
+        username: username,
+        email: email,
+        password: hashPassword
     }).save()
-    res.status(201).json({ data: data, success: true, message: "new user created..." })
-    console.log(newUser);
+    res.status(201).json({
+        success: true,
+        message: "new user created...",
+        id: createUser._id,
+        username: createUser.username,
+        email: createUser.email,
+        password: hashPassword
+        
+     })
+    console.log(createUser);
     
 };
 
 const loginUser = async (req, res) => {
     const { email, password } = req.body
     const data = await User.findOne({ email });
+    // hash compare kore login...
+    const pass = bcrypt.compareSync(password, data.password);
+    if (pass) {
+        console.log("Login successful");
+    } else {
+        console.log("Wrong password");
+    }
+    
+
+    
     if (loginUser) {
-        res.status(200).json({ data: data, success: true, message: "login done..." })
+        res.status(200).json({ success: true, message: "login done..." })
     } else {
         res.status(400).json({ success: false, message: "User not found..." })
     }
 };
 
-module.exports = {createUser, loginUser}
+module.exports = { registrationController, loginUser}
